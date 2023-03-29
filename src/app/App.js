fb0db64ba34daa208653605d78b413f0df28b848
Ilya Dolgov <prhatecore@gmail.com>
2023-03-29 19:20:07 +0300

    routing and recompose app

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-import Users from "./components/users";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navBar";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
+import Users from "./layouts/users";
+// import Users from "./components/users";
 
 function App() {
-  return <Users />;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/login" component={Login} />
+        <Route path="/users" component={Users} />
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;

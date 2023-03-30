ace00e176db7dc647c7dde30198d07b0f9da97c6
Ilya Dolgov <prhatecore@gmail.com>
2023-03-30 11:58:25 +0300

    add reusable component TextField on Login

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
+import TextField from "../components/textField";
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
   const handleChange = ({ target }) => {
     setData((prevState) => ({ ...prevState, [target.name]: target.value }));
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
   return (
-    <form action="">
-      <div>
-        <label htmlFor="email"> Email</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          value={data.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Пароль</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={data.password}
-          onChange={handleChange}
-        />
-      </div>
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Электронная почта"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+      />
+      <TextField
+        label="Пароль"
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 };

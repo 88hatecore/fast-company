1d260b09e969785cfff0b171ca8de267afd9ada7
Ilya Dolgov <prhatecore@gmail.com>
2023-04-17 18:33:45 +0300

    create LocalStorage Service

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import { toast } from "react-toastify";
 import userService from "../services/user.service";
+import { setTokens } from "../services/localStorage.service";
 
 const httpAuth = axios.create();
 const AuthContext = React.createContext();
@@ -11,22 +12,12 @@ export const useAuth = () => {
   return useContext(AuthContext);
 };
 
-const TOKEN_KEY = "jwt-token";
-const REFRESH_KEY = "jwt-refresh-token";
-const EXPIRES_KEY = "jwt-expires";
-
 const AuthProvider = ({ children }) => {
   const [currentUser, setUser] = useState({});
   const [error, setError] = useState(null);
-  function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
-    const expiresDate = new Date().getTime() + expiresIn * 1000;
-    localStorage.setItem(TOKEN_KEY, idToken);
-    localStorage.setItem(REFRESH_KEY, refreshToken);
-    localStorage.setItem(EXPIRES_KEY, expiresDate);
-  }
+
   async function signUp({ email, password, ...rest }) {
-    const key = "AIzaSyCSiY8IXgS8hAB4XiB60l60eIl5LMCXuh8";
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`;
 
     try {
       const { data } = await httpAuth.post(url, {

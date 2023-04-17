2dd18c56e7c630006526ced608ba0ab0be8d6f6f
Ilya Dolgov <prhatecore@gmail.com>
2023-04-17 17:25:48 +0300

    realized SignUp method

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
+const httpAuth = axios.create();
 const AuthContext = React.createContext();
 
 export const useAuth = () => {
@@ -8,7 +10,19 @@ export const useAuth = () => {
 };
 
 const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  async function signUp({ email, password }) {
+    const key = "AIzaSyCSiY8IXgS8hAB4XiB60l60eIl5LMCXuh8";
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
+    const { data } = await httpAuth.post(url, {
+      email,
+      password,
+      returnSecureToken: true
+    });
+    console.log(data);
+  }
+  return (
+    <AuthContext.Provider value={{ signUp }}>{children}</AuthContext.Provider>
+  );
 };
 
 AuthProvider.propTypes = {

99fefb87717c158bc684107ad3bf79ee26f9379b
Ilya Dolgov <prhatecore@gmail.com>
2023-04-23 17:44:44 +0300

    create actions and selectors for Qualities

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,6 @@ import Main from "./layouts/main";
 import Users from "./layouts/users";
 import { ToastContainer } from "react-toastify";
 import { ProfessionProvider } from "../hooks/useProfession";
-import { QualitiesProvider } from "../hooks/useQuality";
 import AuthProvider from "../hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
@@ -22,17 +21,15 @@ function App() {
     <div>
       <AuthProvider>
         <NavBar />
-        <QualitiesProvider>
-          <ProfessionProvider>
-            <Switch>
-              <ProtectedRoute path="/users/:userId?/:edit?" component={Users} />
-              <Route path="/login/:type?" component={Login} />
-              <Route path="/logout" component={LogOut} />
-              <Route path="/" exact component={Main} />
-              <Redirect to="/" />
-            </Switch>
-          </ProfessionProvider>
-        </QualitiesProvider>
+        <ProfessionProvider>
+          <Switch>
+            <ProtectedRoute path="/users/:userId?/:edit?" component={Users} />
+            <Route path="/login/:type?" component={Login} />
+            <Route path="/logout" component={LogOut} />
+            <Route path="/" exact component={Main} />
+            <Redirect to="/" />
+          </Switch>
+        </ProfessionProvider>
       </AuthProvider>
       <ToastContainer />
     </div>
--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Quality from "./quality";
-import { useQualities } from "../../../../hooks/useQuality";
+import { useSelector } from "react-redux";
+import {
+  getQualitiesById,
+  getQualitiesLoadingStatus
+} from "../../../store/qualities";
 
 const QualitiesList = ({ qualities }) => {
-  const { isLoading } = useQualities();
+  const isLoading = useSelector(getQualitiesLoadingStatus());
   if (isLoading) return "Загрузка...";
+  const qualitiesList = useSelector(getQualitiesById(qualities));
+
   return (
     <>
-      {qualities.map((qual) => (
-        <Quality key={qual} id={qual} {...qual} />
+      {qualitiesList.map((qual) => (
+        <Quality key={qual._id} {...qual} />
       ))}
     </>
   );

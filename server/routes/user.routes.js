4bd57f5f21fe6dae9bb7fba597e1be83579b817a
Ilya Dolgov <prhatecore@gmail.com>
2023-04-30 13:23:53 +0300

    Logic for working with users

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,33 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
 
+router.patch("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (userId) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
+router.get("/", async (req, res) => {
+  try {
+    const list = await User.find();
+    res.status(200).send(list);
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
 
 module.exports = router;
